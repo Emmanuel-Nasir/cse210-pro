679c45da26c7652f837a0803cc42f2755dd6bbad
Emmanuel-Nasir <nas24011@byui.edu>
2025-02-02 00:29:08 +0100

    commit and push

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Net;
+using System.Reflection;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.Write("Hello User, Welcome to the magic number game.you are to guess the magic number. It is being chosen randomly 😊");
+        Random randomGuess = new Random();
+        int magicNumber = randomGuess.Next(1,10);
+        int guess = -1;
+        while(guess != magicNumber){
+            Console.WriteLine("Guess the magic number ");
+            guess = int.Parse(Console.ReadLine());
+            
+            if (guess>magicNumber)
+            {
+                Console.Write("You are wrong! your guess is higher");
+            }else if(guess<magicNumber){
+                Console.Write("You are wrong! your guess is lower");
+            }else{
+                Console.Write("You are so lucky, Your guess is correct!");   
+             }
+        }
     }
 }
